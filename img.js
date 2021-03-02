bfab7dbb2f0d564741b7ea5029093b43bea9c14b
Sangita Chadha <sangitachadha@gmail.com>
2021-03-03 00:53:10 +0530

    Add files via upload

---
+++ img.js
@@ -0,0 +1,15 @@
+
+        function incr(n)
+            {
+                n.style.height="400px"
+                n.style.width="400px";
+                n.style.border="dotted";
+                n.style.color="red";
+            }
+        function decr(n)
+            {
+                n.style.height="150px";
+                n.style.width="150px";
+                n.style.border="rgb(46, 14, 224) dashed";
+                
+            }
